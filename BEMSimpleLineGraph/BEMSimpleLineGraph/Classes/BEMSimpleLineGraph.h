8685d030fb21c3b52158453173ade83458256284
magicfeet <magicfeet55@gmail.com>
2018-05-28 23:48:47 -0600

    fix

--- BEMSimpleLineGraph/BEMSimpleLineGraph/Classes/BEMSimpleLineGraph.h
+++ BEMSimpleLineGraph/BEMSimpleLineGraph/Classes/BEMSimpleLineGraph.h
@@ -7,4 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+
+//! Project version number for GraphFramework.
+FOUNDATION_EXPORT double GraphFrameworkVersionNumber;
+
+//! Project version string for GraphFramework.
+FOUNDATION_EXPORT const unsigned char GraphFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GraphFramework/PublicHeader.h>
+
 #import "BEMSimpleLineGraphView.h"
